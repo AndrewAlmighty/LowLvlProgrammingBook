f448878af4db37fe590352e5006ae459b3418f16
AndrewAlmighty <xaidianx@gmail.com>
2018-02-20 20:59:14 +0100

    Finished lib.inc

--- asm/test.py
+++ asm/test.py
@@ -173,30 +173,8 @@ tests=[ Test('string_length',
         xor rdi, rdi
         syscall""", 
         lambda i,o,r: i == o),
-
-        Test('string_copy',
-            lambda v: """
-        section .data
-        arg1: db '""" + v + """', 0
-        arg2: times """ + str(len(v) + 1) +  """ db  66
-        section .text
-        %include "lib.inc"
-        global _start 
-        _start:
-        """ + before_call + """
-        mov rdi, arg1
-        mov rsi, arg2
-        mov rdx, """ + str(len(v) + 1) + """
-        call string_copy
-        """ + after_call + """
-        mov rdi, arg2 
-        call print_string
-        mov rax, 60
-        xor rdi, rdi
-        syscall""", 
-        lambda i,o,r: i == o),
-
-        Test('print_char',
+	   
+	   Test('print_char',
             lambda v:""" section .text
         %include "lib.inc"
         global _start 
@@ -209,8 +187,8 @@ tests=[ Test('string_length',
         xor rdi, rdi
         syscall""", 
         lambda i,o,r: i == o),
-
-        Test('print_uint',
+	   
+	   Test('print_uint',
             lambda v: """section .text
         %include "lib.inc"
         global _start 
@@ -380,6 +358,28 @@ tests=[ Test('string_length',
         mov rax, 60
         syscall""",
         lambda i,o,r: r == 0),
+	   
+	   Test('string_copy',
+            lambda v: """
+        section .data
+        arg1: db '""" + v + """', 0
+        arg2: times """ + str(len(v) + 1) +  """ db  66
+        section .text
+        %include "lib.inc"
+        global _start 
+        _start:
+        """ + before_call + """
+        mov rdi, arg1
+        mov rsi, arg2
+        mov rdx, """ + str(len(v) + 1) + """
+        call string_copy
+        """ + after_call + """
+        mov rdi, arg2 
+        call print_string
+        mov rax, 60
+        xor rdi, rdi
+        syscall""", 
+        lambda i,o,r: i == o),
 
         Test('string_copy_too_long',
             lambda v: """
